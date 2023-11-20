1dc98771e29c3f8d259a5baab66a29e19925ef98
Jai McElroy <50918528+a1708141@users.noreply.github.com>
2023-11-20 13:12:05 +1030

    added files for testing new mesh marker objects

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'multi_robot_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'launch'),glob('launch/*.py')),
+        (os.path.join('share',package_name,'params'),glob('params/*')),
+        (os.path.join('share',package_name,'map'),glob('map/*')),
+        (os.path.join('share',package_name,'rviz'),glob('rviz/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jai',
+    maintainer_email='jai@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
